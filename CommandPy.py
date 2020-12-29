4a4381ff523e25c17378f906dbcb5a9994f31df4
Fred <68379946+Fredfredster@users.noreply.github.com>
2020-12-29 11:12:08 +0000

    Base for commands
    Added a command selector. Now ready to start adding commands. Arguments/tags are detectable and if none are given, error passed.

--- CommandPy.py
+++ CommandPy.py
@@ -14,7 +14,28 @@ import os
 
 current_directory = str(os.path.dirname(os.path.realpath(__file__)))
 
+VALIDCOMMANDS = ['exit']
+VALIDCOMMANDS_SORTED = VALIDCOMMANDS.sort()
+
 print('CommandPy [Version {}]\n(c) 2020 Fred Pashley. All rights reserved.'.format(VERSION))
 
 while True:
 	option = input('\n{}>'.format(current_directory))
+
+	command_split = option.split()
+	if command_split == []:
+		pass
+	else:
+		command = command_split[0]; command_original = command; command = command.lower()
+		try:
+			arguments = command_split[1:]
+		except IndexError as er:
+			arguments = []
+		finally:
+			if command in VALIDCOMMANDS:
+				if command == 'exit':
+					quit()
+				else:
+					pass
+			else:
+				print("'{}' is not recognized as an internal or external command,\noperable program or batch file.".format(command_original))
